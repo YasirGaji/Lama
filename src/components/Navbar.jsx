68a8df11d7972879d267c0cbd69dda7d5fcb65a1
yasir <hellopleasantcodes@gmail.com>
2022-05-25 15:29:34 +0100

    installed styled components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from styled-components;
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
