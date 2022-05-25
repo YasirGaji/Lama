7d06fe8234e1d8a0cc958b1bd78691b760f6a65f
yasir <hellopleasantcodes@gmail.com>
2022-05-25 16:02:29 +0100

    break

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,42 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
-  color: white;
-`
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 
 const Navbar = () => {
   return (
     <Container>
-      Navbar
+      <Wrapper>
+        <Left>
+          LAMA
+        </Left>
+
+        <Center>
+          Nav
+        </Center>
+
+        <Right>
+          Social
+        </Right>
+      </Wrapper>
     </Container>
   )
 }
