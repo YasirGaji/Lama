455a6382cd6e8711b0e2c19db98cb10c9aed9697
yasir <hellopleasantcodes@gmail.com>
2022-06-06 17:59:43 +0100

    done with products page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,17 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Container = styled.div`
 
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  margin: 20px;
+`;
 
-const FilterContainer = styled.div``;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const Filter = styled.div``;
+const Filter = styled.div`
+  margin: 20px;
+`;
 
+const FilterText = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+
+const Option = styled.option`
+
+`;
 
 const ProductList = () => {
   return (
@@ -24,9 +48,47 @@ const ProductList = () => {
         <Title>Outfits</Title>
 
         <FilterContainer>
-          <Filter>filter 1</Filter>
-          <Filter>filter 2</Filter>
+          <Filter>
+            <FilterText>
+              Filter Products:
+            </FilterText>
+
+            <Select>
+              <Option disabled selected>Color</Option>
+              <Option>Red</Option>
+              <Option>Blue</Option>
+              <Option>White</Option>
+              <Option>Black</Option>
+              <Option>Yellow</Option>
+              <Option>Green</Option>
+            </Select>
+
+            <Select>
+              <Option disabled selected>Size</Option>
+              <Option>XS</Option>
+              <Option>S</Option>
+              <Option>M</Option>
+              <Option>L</Option>
+              <Option>XL</Option>
+              <Option>XXL</Option>
+            </Select>
+          </Filter>
+
+          <Filter>
+            <FilterText>
+              Sort by:
+            </FilterText>
+
+            <Select>
+              <Option disabled selected>Newest</Option>
+              <Option>Price (asc)</Option>
+              <Option>Price (desc)</Option>  
+            </Select>
+          </Filter>
         </FilterContainer>
+        <Products />
+        <Newsletter />
+        <Footer />
       </Container>
     </>
   )
