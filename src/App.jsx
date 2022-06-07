6a84793e9780ed72d3144fa6665afbe8e7d6bcca
yasir <hellopleasantcodes@gmail.com>
2022-06-07 21:35:00 +0100

    created the login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <Login />
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.5)
+  ),
+  url("https://images.unsplash.com/photo-1630750793250-71173d8bfa5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmFzaGlvbiUyMGVjb21tZXJjZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=700&q=60");
+
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: #fff;
+`;
+
+const Title = styled.h1`
+  Font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #fff;
+  border: 1px solid #3F51B5;
+  margin-bottom: 10px;
+  &:hover {
+    background-color: #3F51B5;
+  }
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 
 const Login = () => {
   return (
     <Container>
-    
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form> 
+          <Input type="text" placeholder="Username" />
+          <Input type="text" placeholder="Password" />
+          <Button>LOGIN</Button>
+
+          <Link>FORGOT PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+        
+      </Wrapper>
     </Container>
   )
 }
