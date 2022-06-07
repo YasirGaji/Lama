3cb71347f012923a576aa047f35395feda3b1658
yasir <hellopleasantcodes@gmail.com>
2022-06-07 21:11:39 +0100

    created the register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Register />
     </div>
   );
 };
