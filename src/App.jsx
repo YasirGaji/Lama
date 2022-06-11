683342a3226acdba4ed1e07dd54c9b265facccbd
yasir <hellopleasantcodes@gmail.com>
2022-06-11 13:47:35 +0100

    worked on the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
@@ -8,7 +10,14 @@ import Cart from "./pages/Cart";
 const App = () => {
   return (
     <div>
-      <Product />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products-all" element={<ProductList />} />
+        <Route exact path="/product" element={<Product />} />
+        <Route exact path="/signup" element={<Register />} />
+        <Route exact path="/signin" element={<Login />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 };
